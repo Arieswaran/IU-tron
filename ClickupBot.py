16e25e67b4e3e0070a12ece8c992c43238c7d97e
Arieswaran <arieswaran@gmail.com>
2023-01-15 13:23:08 +0530

    Squashed those commits like a bug (that nobody needs to know about).
    Squashed all my embarrassing typos and spelling mistakes, you're welcome future self.
    
    Also Note to future self : Don't fcking commit tokens and passwords. Thank you 💖

---
+++ ClickupBot.py
@@ -0,0 +1,248 @@
+import requests
+import discord
+import pyclickup
+import os
+from discord.ext import commands
+from discord import ButtonStyle
+from discord.ui import Button
+import enum
+import openai
+import Levenshtein
+import time
+
+token = os.getenv("DISCORD_NATASHA_TOKEN")
+#get read messages and send messages permissions
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix=["n","N"],help_command=None, case_insensitive = True, intents=intents)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+#clickup
+clickup_token = os.getenv("CLICKUP_TOKEN")
+clickup = pyclickup.ClickUp(clickup_token)
+main_team = clickup.teams[0]
+main_space = main_team.spaces[0]
+main_project = main_space.projects[2]
+bug_list = main_project.lists[1]
+statuses_list = main_project.statuses
+all_tasks = bug_list.get_all_tasks()
+
+def updateAllTasks():
+    global all_tasks
+    all_tasks = bug_list.get_all_tasks()
+
+def getTask(task_id):
+    for task in all_tasks:
+        if task.id == task_id:
+            return task
+    return None
+
+def createTask(name, description):
+    task = bug_list.create_task(name, description)
+    updateAllTasks()
+    return getTask(task)
+
+
+def getTaskTitle(message):
+    a = "Make a clickup task title less than 13 words for the below message\n"
+    a = a + message
+    res = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=a,
+        max_tokens=512,
+        temperature=0
+    )
+    return "[BUG] "+ res["choices"][0]["text"].strip()
+
+def getTaskDescription(message):
+    a = "Make a bug report for the below message\n"
+    a = a + message
+    res = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=a,
+        max_tokens=1000,
+        temperature=0
+    )
+    return res["choices"][0]["text"].strip()
+
+
+def similarity_check(title1, title2, threshold):
+    lev_similarity = Levenshtein.ratio(title1, title2)
+    title1_words = set(title1.split())
+    title2_words = set(title2.split())
+    jac_similarity = len(title1_words & title2_words) / \
+        len(title1_words | title2_words)
+    if lev_similarity >= threshold or jac_similarity >= threshold:
+        return True
+    else:
+        return False
+
+class Priority(enum.Enum):
+    """task priority enum"""
+
+    NONE = 0
+    URGENT = 1
+    HIGH = 2
+    NORMAL = 3
+    LOW = 4
+
+
+@bot.event
+async def on_ready():
+    activity = discord.Activity(
+        type=discord.ActivityType.playing, name="Hitwicket Superstars")
+    await bot.change_presence(activity=activity)
+    print("We have logged in as {0.user}".format(bot))
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if message.mention_everyone:
+        return
+    ctx = await bot.get_context(message)
+    is_cmd = ctx.valid
+    if(is_cmd):
+        await bot.process_commands(message)
+    elif bot.user.mentioned_in(message):
+        message.content = message.content.replace(bot.user.mention, "")
+        if message.reference != None:
+            if(bot.user.mentioned_in(message.reference.resolved)):
+                return
+            if(message.reference.resolved.author == bot.user):
+                return
+            message.content = message.reference.resolved.content
+            message.attachments = message.reference.resolved.attachments
+        if message.content == "" and len(message.attachments) == 0:
+            await ctx.reply("Please provide a message to make a task for")
+            return
+            
+        title = ""
+        if(message.content != ""):
+            title = getTaskTitle(message.content)
+        similar_tasks = []
+        for task in all_tasks:
+            if(similarity_check(task.name, title, 0.8)):
+                similar_tasks.append(task)
+        
+        if(len(similar_tasks) > 0):
+            view = DuplicateTask(message,title,message.content,message.attachments)
+            embed = discord.Embed(title="Duplicate Tasks", description="The following tasks are similar to the one you are trying to create. Do you still want to create a new task?", color=0x00ff00)
+            for task in similar_tasks:
+                embed.add_field(name=task.name, value=task.url, inline=False)
+            await ctx.reply(embed=embed,view = view)
+            return
+        
+        description = ""
+        if(message.content != ""):
+            description = getTaskDescription(message.content)
+        attachement_links = [attachement.url for attachement in message.attachments]
+        if(len(attachement_links) > 0):
+            description = description + "\n\nAttachments:\n" + "\n".join(attachement_links)
+        task = createTask(title, description)
+        if task == None:
+            await ctx.reply("Error creating task")
+            return
+        view = ClickupTask(task)
+        embed = discord.Embed(title=task.name, description=task.description, color=0x00ff00)
+        embed.add_field(name="Status", value=task.status.status, inline=True)
+        embed.add_field(name="Priority", value= "None", inline=True) #hack
+        view.add_item(discord.ui.Button(label="Open in Clickup",style=ButtonStyle.link,url=task.url))
+        await ctx.reply(embed=embed,view = view)
+
+class DuplicateTask(discord.ui.View):
+    def __init__(self,og_msg,title,message_content,attachments):
+        super().__init__()
+        self.title = title
+        self.message_content = message_content
+        self.attachments = attachments
+        self.og_msg = og_msg
+    
+    @discord.ui.button(label="Yes",style=ButtonStyle.green)
+    async def yes(self,button:discord.ui.Button,interaction:discord.Interaction):
+        await interaction.response.defer()
+        button.disabled = True
+        description = ""
+        if (self.message_content != ""):
+            description = getTaskDescription(self.message_content)
+        attachement_links = [attachement.url for attachement in self.attachments]
+        if (len(attachement_links) > 0):
+            description = description + "\n\nAttachments:\n" + "\n".join(attachement_links)
+        task = createTask(self.title, description)
+        if task == None:
+            await interaction.response.send_message("Error creating task")
+            return
+        view = ClickupTask(task)
+        embed = discord.Embed(title=task.name, description=task.description, color=0x00ff00)
+        embed.add_field(name="Status", value=task.status.status, inline=True)
+        embed.add_field(name="Priority", value="None", inline=True)  # hack
+        view.add_item(discord.ui.Button(label="Open in Clickup", style=ButtonStyle.link, url=task.url))
+        #send message in the channel
+        await interaction.message.delete()
+        ctx = await bot.get_context(self.og_msg)
+        await ctx.reply(embed=embed,view = view)
+
+    @discord.ui.button(label="No",style=ButtonStyle.red)
+    async def no(self,button:discord.ui.Button,interaction:discord.Interaction):
+        await interaction.message.edit(view=None)
+        await interaction.response.defer()
+
+
+class ClickupTask(discord.ui.view.View):
+    def __init__(self,task):
+        super().__init__()
+        self.task = task
+
+    # @discord.ui.select(
+    #     placeholder="Change Status",
+    #     options=[discord.SelectOption(
+    #         label=status.status, value=status.status) for status in statuses_list]
+    # ) 
+
+    # async def change_status(self,select:discord.ui.Select,interaction:discord.Interaction):
+    #     self.task.status = select.values[0]
+    #     self.task.update(status = select.values[0])
+    #     await self.updateTaskDetails(interaction)
+    #     #update embed
+    
+    # async def updateTaskDetails(self, interaction: discord.Interaction):
+    #     embed = discord.Embed(title=self.task.name,
+    #                           description=self.task.description, color=0x00ff00)
+    #     embed.add_field(name="Status", value=self.task.status, inline=True)
+    #     priority = self.task.priority
+    #     if(priority != None):
+    #         priority = Priority(int(priority)).name
+    #     else:
+    #         priority = "None"
+    #     embed.add_field(name="Priority", value= priority, inline=True)
+    #     await interaction.response.edit_message(embed=embed)
+        
+
+    # @discord.ui.select(
+    #     placeholder="Set Priority",
+    #     options=[discord.SelectOption(
+    #         label=Priority(priority).name, value= str(priority)) for priority in range(1,5)]
+    # )
+
+    # async def set_priority(self,select:discord.ui.Select,interaction:discord.Interaction):
+    #     self.task.priority = select.values[0]
+    #     self.task.update(priority = int(select.values[0]))
+    #     await self.updateTaskDetails(interaction)
+
+# command error
+@bot.event
+async def on_command_error(ctx, error):
+    return # will remove if we add commands later
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Command not found")
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Missing required argument")
+    elif isinstance(error, commands.MissingPermissions):
+        await ctx.send("You do not have permission to use this command")
+    else:
+        await ctx.send("An error occured")
+        raise error
+    
+
+
+bot.run(token)
---
+++ README.md
@@ -0,0 +1,22 @@
+## IU-tron
+
+Welcome to IU-tron, where bugs go to die! Just kidding, they go to ClickUp, but that's almost the same thing, right?
+
+## AI-powered bug reporting
+
+First up, we have the AI-powered bug reporting feature. Just tag IU-tron in your message, and it will automatically create a bug report for you. No more manual labor! (Unless you count typing out the tag as manual labor, but let's not go there.)
+
+## Duplicate task detection
+
+But wait, there's more! IU-tron also has a built-in duplication checker, because we all know how annoying it is to have multiple tasks for the same issue. It's like having a party and inviting the same person twice. Just no. The bot uses the Levenshtein distance and Jaccard similarity to detect any duplicates, and will ask you if you still want to proceed with creating the task. It's like having a personal assistant, but for bug reports.
+
+## Task management
+
+And speaking of personal assistants, IU-tron will send you all the details of the created task in a handy-dandy discord message. No more scrambling around trying to find where you put that task, it's all there for you. Plus, you can change the status and priority of the task right from the comfort of your discord server. It's like having a remote control for your tasks.
+
+## Conclusion
+
+IU-tron is the bot you never knew you needed, like that extra slice of pizza you thought you couldn't finish but somehow found room for. With its AI bug reporting, duplication checking, and task management capabilities, you'll wonder how you ever lived without it. And who knows, maybe someday it'll become sentient and take over the world. But until then, enjoy the convenience of IU-tron on your discord server!
+
+
+
