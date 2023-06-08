6b2e911a462cd320a64ece3851ef0053fc70f155
Arieswaran <arieswaran@gmail.com>
2023-06-09 04:04:11 +0530

    oh well, different types for different games. Todo : make it dynamic if not lazy

--- ClickupBot.py
+++ ClickupBot.py
@@ -121,12 +121,12 @@ async def on_message(message):
             embed = message.embeds[0]
             msg = embed.description
             msg = msg.split("Hitwicket Cricket Games")[1].strip()
-            rating, title, description = msg.split("\n")
+            rating, description = msg.split("\n")
             by = embed.fields[0].value
             if (len(description.split(" ")) > 4 or len(description) > 20): # filter out reviews with less than 4 words in description or less than 20 characters
                 channel = bot.get_channel(1070598072104656906) # channel where we want to post the filtered reviews
                 embed = discord.Embed(
-                    title=title, description=rating + "\n" + description + "\n" + by)
+                    description=rating + "\n" + description + "\n" + by)
                 await channel.send(embed=embed)
         return
     ctx = await bot.get_context(message)
